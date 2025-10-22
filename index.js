34d0c4b41b0a41eb329f7fea1e32529d90af6f18
Thane <thabay0606@students.maquoketaschools.org>
2025-10-22 14:21:49 -0500

    Added automobiles variable, added airplane and skateboard to end and bike and helicopter to front. Removed the first and last element in a new variable named automobilesCopy.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+var automobiles = ["car", "truck", "motorcycle"];
+automobiles.push("airplane", "skateboard");
+automobiles.unshift("bike", "helicopter");
 
+var automobilesCopy = automobiles;
+automobilesCopy.pop();
+automobilesCopy.shift();
+
+document.getElementById("result").innerHTML = automobiles;
